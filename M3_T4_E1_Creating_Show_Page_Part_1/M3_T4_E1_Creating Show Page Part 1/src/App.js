0ff10aca315cc5a86a8ee477d47f2e64c7275065
saikumard <dsaikumar357@gmail.com>
2021-07-02 10:48:59 +0530

    added show page

--- M3_T4_E1_Creating_Show_Page_Part_1/M3_T4_E1_Creating Show Page Part 1/src/App.js
+++ M3_T4_E1_Creating_Show_Page_Part_1/M3_T4_E1_Creating Show Page Part 1/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Show from './pages/Show';
 import Starred from './pages/Starred';
+import Show from './pages/Show';
 
 function App() {
   return (
@@ -16,12 +16,12 @@ function App() {
         <Starred />
       </Route>
 
-      <Route exact path="/show/:idx">
+      <Route exact path="/show/:id">
         <Show />
       </Route>
 
       <Route>
-        <div>404 not found</div>
+        <div>Not found</div>
       </Route>
     </Switch>
   );
